c6e3e03c66e44a7242ed9f2a69d79cd8b4dabb04
ashuthe1 <ashutosh251848@gmail.com>
2023-07-17 01:17:12 +0530

    Added all 3 urls as origin to cors

--- server/server.js
+++ server/server.js
@@ -16,7 +16,7 @@ connectDB(MONGO_URL);
 
 app.use(
   cors({
-    origin: "https://careerhelp.vercel.app",
+    origin: ["https://careerhelp.vercel.app", "https://careerhelp.netlify.app", "http://localhost:5173"], // <-- location of the react app were connecting to
     methods: ["GET", "POST", "PATCH", "DELETE"],
     credentials: true,
   })
