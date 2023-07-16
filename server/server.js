698be4b75b19002074f9052d974cd0ae34b75fba
ashuthe1 <ashutosh251848@gmail.com>
2023-07-17 00:44:23 +0530

    Changed Cors Origin URL

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const app = express();
 const cors = require("cors");
 require("dotenv").config();
 const cookieParser = require( "cookie-parser");
@@ -13,33 +14,31 @@ const MONGO_URL = process.env.MONGO_URL || "mongodb://0.0.0.0:27017/careerhelper
 
 connectDB(MONGO_URL);
 
-const app = express();
-app.use(express.json());
-app.use(cookieParser());
 app.use(
-    cors({
-      origin: ["https://careerhelp.vercel.app/", "http://localhost:5173"],
-      methods: ["GET", "POST", "PATCH", "DELETE"],
-      credentials: true,
-    })
+  cors({
+    origin: "https://careerhelp.vercel.app",
+    methods: ["GET", "POST", "PATCH", "DELETE"],
+    credentials: true,
+  })
 );
 
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "OPTIONS, GET, POST, PUT, PATCH, DELETE"
-  );
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  if (req.method === "OPTIONS") {
-    return res.sendStatus(200);
-  }
-  next();
-});
+app.use(express.json());
+app.use(cookieParser());
 
-app.use(authRoute);
-app.use(userRoute);
-app.use(jobRoute);
+// app.use((req, res, next) => {
+//   res.setHeader("Access-Control-Allow-Origin", "*");
+//   res.setHeader(
+//     "Access-Control-Allow-Methods",
+//     "OPTIONS, GET, POST, PUT, PATCH, DELETE"
+//   );
+//   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+//   if (req.method === "OPTIONS") {
+//     return res.sendStatus(200);
+//   }
+//   next();
+// });
+
+app.use(authRoute, userRoute, jobRoute);
 
 
 app.listen(PORT, () => {
