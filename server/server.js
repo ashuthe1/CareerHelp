a4687c5ef11dbfee3f032a33b24a2979da935cf4
ashuthe1 <ashutosh251848@gmail.com>
2023-07-16 16:21:57 +0530

    Updated Server.js

--- server/server.js
+++ server/server.js
@@ -2,8 +2,11 @@ const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 const cookieParser = require( "cookie-parser");
+
 const connectDB = require( "./database/connection");
 const authRoute = require("./routes/authRoute");
+const userRoute = require("./routes/userRoute");
+const jobRoute = require("./routes/jobRoute");
 
 const PORT = process.env.PORT || 5000;
 const MONGO_URL = process.env.MONGO_URL || "mongodb://0.0.0.0:27017/careerhelper";
@@ -16,6 +19,9 @@ app.use(cookieParser());
 
 
 app.use(authRoute);
+app.use(userRoute);
+app.use(jobRoute);
+
 
 app.listen(PORT, () => {
   console.log(`CareerHelper Server is Running on ${PORT} `);
